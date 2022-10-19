e0a664cab7466b3bb481e3e522883823681867af
captivea-wsarah <sarah.wilson@captivea.us>
2022-10-19 12:00:43 -0400

    [Update]
    correction to day 8 part 1

--- odoo_academy/models/sale_order.py
+++ odoo_academy/models/sale_order.py
@@ -5,8 +5,8 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    session_id = fields.Many2one(comodel_name='academy.session', string='Related Session', ondelete='set null')
+    session_id = fields.Many2one(comodel_name='academy.session',string='Related Session',ondelete='set null')
 
-    instructor_id = fields.Many2one(string='Session Instructor', related='session_id_instructor')
+    instructor_id = fields.Many2one(string='Session Instructor',related='session_id_instructor')
 
-    student_ids = fields.Many2many(string='Students', related='session_id.student_ids')
+    student_ids = fields.Many2many(string='Students',related='session_id.student_ids')
